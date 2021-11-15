0251acb41bf762f9c4c38f53f8a91066580ff4bc
한상선 <hs94122@naver.com>
2021-11-15 15:40:41 +0900

    java-1

---
+++ src/com/javaex/study01/Ex10_lotto.java
@@ -0,0 +1,54 @@
+package com.javaex.study01;
+
+public class Ex10_lotto {
+	public static void main(String[] args) {
+		System.out.println("자동로또 입니다.");
+			System.out.println("번호를 자동으로 선택합니다");
+			int num01_01 =(int)(Math.random()*45)+1;
+			int num01_02 =(int)(Math.random()*45)+1;
+			int num01_03 =(int)(Math.random()*45)+1;
+			int num01_04 =(int)(Math.random()*45)+1;
+			int num01_05 =(int)(Math.random()*45)+1;
+			int num01_06 =(int)(Math.random()*45)+1;
+			int num02_01 =(int)(Math.random()*45)+1;
+			int num02_02 =(int)(Math.random()*45)+1;
+			int num02_03 =(int)(Math.random()*45)+1;
+			int num02_04 =(int)(Math.random()*45)+1;
+			int num02_05 =(int)(Math.random()*45)+1;
+			int num02_06 =(int)(Math.random()*45)+1;
+			int num03_01 =(int)(Math.random()*45)+1;
+			int num03_02 =(int)(Math.random()*45)+1;
+			int num03_03 =(int)(Math.random()*45)+1;
+			int num03_04 =(int)(Math.random()*45)+1;
+			int num03_05 =(int)(Math.random()*45)+1;
+			int num03_06 =(int)(Math.random()*45)+1;
+			int num04_01 =(int)(Math.random()*45)+1;
+			int num04_02 =(int)(Math.random()*45)+1;
+			int num04_03 =(int)(Math.random()*45)+1;
+			int num04_04 =(int)(Math.random()*45)+1;
+			int num04_05 =(int)(Math.random()*45)+1;
+			int num04_06 =(int)(Math.random()*45)+1;
+			int num05_01 =(int)(Math.random()*45)+1;
+			int num05_02 =(int)(Math.random()*45)+1;
+			int num05_03 =(int)(Math.random()*45)+1;
+			int num05_04 =(int)(Math.random()*45)+1;
+			int num05_05 =(int)(Math.random()*45)+1;
+			int num05_06 =(int)(Math.random()*45)+1;
+			int num06_01 =(int)(Math.random()*45)+1;
+			int num06_02 =(int)(Math.random()*45)+1;
+			int num06_03 =(int)(Math.random()*45)+1;
+			int num06_04 =(int)(Math.random()*45)+1;
+			int num06_05 =(int)(Math.random()*45)+1;
+			int num06_06 =(int)(Math.random()*45)+1;
+			
+			System.out.println("당신의 번호\n"+num01_01+" "+num01_02+" "+num01_03+" "+num01_04+" "+num01_05+" "+num01_06);
+			System.out.println(num02_01+" "+num02_02+" "+num02_03+" "+num02_04+" "+num02_05+" "+num02_06);
+			System.out.println(num03_01+" "+num03_02+" "+num03_03+" "+num03_04+" "+num03_05+" "+num03_06);
+			System.out.println(num04_01+" "+num04_02+" "+num04_03+" "+num04_04+" "+num04_05+" "+num04_06);
+			System.out.println(num05_01+" "+num05_02+" "+num05_03+" "+num05_04+" "+num05_05+" "+num05_06);
+			System.out.println("이번주 결과는");
+			System.out.println(num06_01+" "+num06_02+" "+num06_03+" "+num06_04+" "+num06_05+" "+num06_06);
+		
+		}
+		
+	}
