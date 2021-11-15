2c3c3ce1c4aecec411754eb0139e20f97177cd05
한상선 <hs94122@naver.com>
2021-11-15 17:58:11 +0900

    java-3

---
+++ src/com/javaex/ex03/Ex04.java
@@ -0,0 +1,24 @@
+package com.javaex.ex03;
+
+import java.util.Scanner;
+
+public class Ex04 {
+
+	public static void main(String[] args) {
+		//조건문 연습문제-1
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("근무시간:");
+		int time = sc.nextInt();
+		if(time<=8) {
+			System.out.println("임금은 "+(time*10000)+"원 입니다.");
+		} else {
+			int pay = ((time-8)*12000)+80000;
+			System.out.println("임금은 "+pay+"원 입니다.");
+		}
+		
+		sc.close();
+		
+	}
+
+}
