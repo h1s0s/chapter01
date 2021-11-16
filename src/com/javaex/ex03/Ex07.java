13464a1e38caac0a9561040d21ab29d7fa496611
한상선 <hs94122@naver.com>
2021-11-16 10:39:54 +0900

    java-3

--- src/com/javaex/ex03/Ex07.java
+++ src/com/javaex/ex03/Ex07.java
@@ -11,11 +11,11 @@ public class Ex07 {
 		int point = sc.nextInt();
 		if (point<60) {
 			System.out.println("F등급");
-		} else if (point<69) {
+		} else if (point<=69) {
 			System.out.println("D등급");
-		} else if (point<79) {
+		} else if (point<=79) {
 			System.out.println("C등급");
-		} else if (point<89) {
+		} else if (point<=89) {
 			System.out.println("B등급");
 		} else {
 			System.out.println("A등급");
