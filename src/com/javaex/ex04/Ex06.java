6ae998f336f7fdd1c25d6aaa033cf6a5c6b5c27c
한상선 <hs94122@naver.com>
2021-11-17 14:54:10 +0900

    2021-11-17 14:54

---
+++ src/com/javaex/ex04/Ex06.java
@@ -0,0 +1,15 @@
+package com.javaex.ex04;
+
+public class Ex06 {
+
+	public static void main(String[] args) {
+		
+		for (int dan=2; dan<10; dan++) {
+			for (int i=1; i<10; i++) {
+				System.out.println(dan+ "*" + i + "=" + dan*i);
+			}
+			int i=1;
+		}
+	}
+
+}
