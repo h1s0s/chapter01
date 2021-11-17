be28835514ba09f687b3c2e3f591aa7646367bf9
한상선 <hs94122@naver.com>
2021-11-17 15:42:09 +0900

    2021-11-17 15:42

---
+++ src/com/javaex/ex04/Ex07.java
@@ -0,0 +1,15 @@
+package com.javaex.ex04;
+
+public class Ex07 {
+
+	public static void main(String[] args) {
+		for(int x=0; x<6; x++) {
+			System.out.print("*");
+			for(int y=0; y<=5; y++) {
+				System.out.print("*");
+			} System.out.println("");
+		}
+	}
+
+}
+
