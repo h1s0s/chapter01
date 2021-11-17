33cf4d4c5358a9748e379d3b7928055897b0da33
한상선 <hs94122@naver.com>
2021-11-17 11:09:28 +0900

    2021-11-17 11:09

---
+++ src/com/javaex/ex04/Ex01.java
@@ -0,0 +1,16 @@
+package com.javaex.ex04;
+
+public class Ex01 {
+
+	public static void main(String[] args) {
+
+		int i=0;//초기화
+		
+		while(i<5) {//조건문
+			System.out.println("I like java"+i);//반복문
+			i++;//증감식
+		}
+
+	}
+
+}
